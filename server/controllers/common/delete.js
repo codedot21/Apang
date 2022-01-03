9ca6c16b40fb6629c0838853a1a5130703b767ba
RiaOh <ken5840714@gmail.com>
2022-01-03 16:05:33 +0900

    Feat: Update common/delete.js,sign.js,signout.js, models/index.js-config.js,tokenFunctions/index.js feature

--- server/controllers/common/delete.js
+++ server/controllers/common/delete.js
@@ -1,1 +1,9 @@
-module.exports = (req, res) => {};
+const { users } = require("../../models");
+const { isAuthorized } = require("../tokenFunctions");
+
+module.exports = async (req, res) => {
+  await users.destroy({
+    where: { id: req.body.id },
+  });
+  res.status(200).send({ message: "Delete OK" });
+};
