befe3a99522871897fa7df0bbe552e6e94e25bae
RiaOh <ken5840714@gmail.com>
2022-01-03 14:03:20 +0900

    Feat: Add sequelize, tokenFunctions feature

---
+++ server/models/qna.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class qna extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  qna.init({
+    title: DataTypes.INTEGER,
+    content: DataTypes.STRING,
+    users_id: DataTypes.INTEGER,
+    category: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'qna',
+  });
+  return qna;
+};
---
+++ server/models/reviews.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class reviews extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  reviews.init({
+    users_id: DataTypes.INTEGER,
+    receipts_img: DataTypes.STRING,
+    content: DataTypes.STRING,
+    d_name: DataTypes.STRING,
+    hospital_name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'reviews',
+  });
+  return reviews;
+};
