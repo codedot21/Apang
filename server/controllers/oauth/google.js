96d2ed17d82a7297a25d44267cd1328c5cb83375
gusdnvkfks <kbs2415@naver.com>
2022-01-23 13:46:16 +0900

    Feat Deploy

--- server/controllers/oauth/google.js
+++ server/controllers/oauth/google.js
@@ -2,15 +2,13 @@ const axios = require("axios");
 const jwtDecode = require("jwt-decode");
 const clientID = process.env.GOOGLE_CLIENT_ID;
 const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
-const { users } = require("../../models");
 
 module.exports = async (req, res) => {
   // console.log("구글인포", req.body);
   await axios
     .post("https://oauth2.googleapis.com/token", {
-      client_id:
-        "969351325951-bjednmg05v39vndlf4fk7re3oi3426nv.apps.googleusercontent.com",
-      client_secret: "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",
+      client_id: clientID,
+      client_secret: clientSecret,
       code: req.body.authorizationCode,
       grant_type: "authorization_code",
       redirect_uri: "https://localhost:3000",
